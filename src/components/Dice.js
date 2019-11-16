a71eb2f9687e57592058a497bf32d50da6a6622c
pavkrol <pdkrol@gmail.com>
2019-11-16 17:57:33 +0100

    add Dice component, prepare functions for holding and rolling selected dice

---
+++ src/components/Dice.js
@@ -0,0 +1,143 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const DiceWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #fff;
+  padding: 10px;
+  display: grid;
+  grid-gap: 10px;
+  border-radius: 10px;
+  position: relative;
+  border: 2px solid #FFF;
+  ${props =>
+    props.hold &&
+    css`
+      border: 2px solid red;
+    `}
+  ${props =>
+    props.value === 1 &&
+    css`
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr;
+    `}
+  ${props =>
+    props.value === 2 &&
+    css`
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+    `}
+  ${props =>
+    (props.value === 3 || props.value === 5) &&
+    css`
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 1fr 1fr 1fr;
+    `}
+  ${props =>
+    props.value === 4 &&
+    css`
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+    `}
+  ${props =>
+    props.value === 6 &&
+    css`
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+    `}
+  div {
+    display: none;
+    background-color: #000;
+    width: 10px;
+    height: 10px;
+    justify-self: center;
+    align-self: center;
+    border-radius: 50%;
+    ${props =>
+      props.value === 1 &&
+      css`
+        :nth-of-type(1) {
+          display: block;
+        }
+      `}
+    ${props =>
+      props.value === 2 &&
+      css`
+        :nth-of-type(1),
+        :nth-of-type(2) {
+          display: block;
+        }
+        :nth-of-type(2) {
+          grid-column: 2 / 3;
+          grid-row: 2 / 3;
+        }
+      `}
+    ${props =>
+      props.value === 3 &&
+      css`
+        :nth-of-type(1),
+        :nth-of-type(2),
+        :nth-of-type(3) {
+          display: block;
+        }
+        :nth-of-type(2) {
+          grid-column: 2 / 3;
+          grid-row: 2 / 3;
+        }
+        :nth-of-type(3) {
+          grid-column: 3 / 4;
+          grid-row: 3 / 4;
+        }
+      `}
+    ${props =>
+      props.value === 4 &&
+      css`
+        :nth-of-type(1),
+        :nth-of-type(2),
+        :nth-of-type(3),
+        :nth-of-type(4) {
+          display: block;
+        }
+      `}
+    ${props =>
+      props.value === 5 &&
+      css`
+        :nth-of-type(1),
+        :nth-of-type(2),
+        :nth-of-type(3),
+        :nth-of-type(4),
+        :nth-of-type(5) {
+          display: block;
+        }
+        :nth-of-type(2),
+        :nth-of-type(5) {
+          grid-column: 3 / 4;
+        }
+        :nth-of-type(3) {
+          grid-column: span 3;
+        }
+      `}
+    ${props =>
+      props.value === 6 &&
+      css`
+        display: block;
+      `}
+    
+  }
+`;
+
+const Dice = ({ value, hold, hold_dice, number }) => {
+  return (
+    <DiceWrapper onClick={() => hold_dice(number)} value={value} hold={hold}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </DiceWrapper>
+  );
+};
+
+export default Dice;
--- src/containers/GameView.js
+++ src/containers/GameView.js
@@ -1,16 +1,100 @@
-import React from "react";
+import React, { useReducer } from "react";
 import styled from "styled-components";
+import Dice from "../components/Dice";
 
 const GameViewWrapper = styled.section`
-  color: black;
+  padding: 100px 0;
+  background: radial-gradient(50% 50% at 50% 50%, #1e5c94 0%, #223b52 100%);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    padding: 20px 85px;
+    font-family: "Carter One", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 52px;
+    line-height: 60px;
+    position: relative;
+    background-image: url(${props => props.image});
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  h2 {
+    font-family: "Share", sans-serif;
+    font-size: 24px;
+    line-height: 27px;
+    margin-bottom: 50px;
+  }
+`;
+
+const Board = styled.div`
+  width: 900px;
+  height: 650px;
+  background: radial-gradient(50% 50% at 50% 50%, #007d51 0%, #00603e 100%);
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  padding: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 const GameView = ({ extremeDifficulty, sequenceGame }) => {
+  const roll_dice = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
+  const initialState = [
+    { hold: false, value: roll_dice() },
+    { hold: false, value: roll_dice() },
+    { hold: false, value: roll_dice() },
+    { hold: false, value: roll_dice() },
+    { hold: false, value: roll_dice() },
+    { hold: false, value: roll_dice() }
+  ];
+
+  const [dice_set, dispatch] = useReducer(reducer, initialState);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "roll":
+        state[action.index].value = roll_dice();
+        return [...state];
+      case "hold":
+        state[action.index].hold = !state[action.index].hold;
+      default:
+        return [...state];
+    }
+  }
+
+  const roll_dice_set = () => {
+    dice_set.forEach((dice, index) => {
+      !dice.hold && dispatch({ type: "roll", index: index });
+    });
+  };
+
+  const hold_dice = index => {
+    dispatch({ type: "hold", index: index });
+  };
+
   return (
     <GameViewWrapper>
-      <h1>Game</h1>
-      <p>Difficulty: {extremeDifficulty ? "Extreme" : "Normal"}</p>
-      <p>Game Type: {sequenceGame ? "In sequence" : "Standard"}</p>
+      <h1>Yahtzee</h1>
+      <h2>Dice Game</h2>
+      <Board>
+        <button onClick={() => roll_dice_set()}>Roll</button>
+        {dice_set.map((dice, index) => (
+          <Dice
+            hold_dice={hold_dice}
+            number={index}
+            key={index}
+            value={dice.value}
+            hold={dice.hold}
+          />
+        ))}
+      </Board>
     </GameViewWrapper>
   );
 };
